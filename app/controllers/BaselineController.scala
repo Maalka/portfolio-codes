037048aeabfe1104157ee32b0957b5ed18646b4d
Rimas Gulbinas <rimas.gulbinas@gmail.com>
2018-08-03 17:24:56 -0400

    prescriptive resource dropdown formatted

--- app/controllers/BaselineController.scala
+++ app/controllers/BaselineController.scala
@@ -295,7 +295,7 @@ class BaselineController @Inject() (val cache: AsyncCacheApi, cc: ControllerComp
             },
             "climate_zone": {
               "type": "string",
-              "enum": ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"],
+              "enum": ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"],
               "required": true
             },
             "file_id": {
--- app/models/Solar.scala
+++ app/models/Solar.scala
@@ -130,7 +130,7 @@ case class SolarProperties(parameters: JsValue) {
     }
     val climateZone = {
         solarResources.climate_zone match {
-          case Some(a) if List("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16").contains(a) => a
+          case Some(a) if List("0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16").contains(a) => a
           case _ => throw new Exception("Not a valid Climate Zone for given Prescriptive Resource! ")
         }
     }
