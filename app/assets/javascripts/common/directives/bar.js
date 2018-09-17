574d86ad25709954aadf399d2de0c32e6dfd036b
Ariel Kotch <arielkotch@Ariels-Air.fios-router.home>
2018-09-17 10:17:32 -0400

    random color list added

--- app/assets/javascripts/common/directives/bar.js
+++ app/assets/javascripts/common/directives/bar.js
@@ -24,13 +24,12 @@ define(['angular', 'highcharts', './main'], function(angular) {
         options: '='
 
       },
-      template: '<div style="width:100%;height:600px;"></div>',
+      template: '<div style="width:100%;height:100%;"></div>',
       link: function(scope, element) {
 
         var options = {
           chart: {
-            type: 'bar',
-            margin: 100
+            type: 'bar'
 
           },
           labels: {
@@ -52,7 +51,7 @@ define(['angular', 'highcharts', './main'], function(angular) {
             symbolHeight: 10,
             itemDistance: 10,
             padding: 3,
-            width: 500,
+
             itemStyle: {
               fontSize: '10px',
               fontWeight: 100,
@@ -169,14 +168,20 @@ define(['angular', 'highcharts', './main'], function(angular) {
         $scope.energyUse = energyUse;
         var series=[];
 
+        var colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+        		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+        		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+        		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+        		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+        		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+        		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+        		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+        		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+        		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
 
-        function getColor(){
-          var colors=['#ec7b7d','#FDC168','#91d1be','#768FDf','#fddb64','#8e83bc','#F36993','#ec7b7d','#FDC168','#91d1be'];
-          var rand=Math.floor((Math.random() * 9) + 0);
-          return colors[rand];
-        }
-
+var index;
         function createSeries() {
+          index=0;
 
           for (var propEui in eui) {
             if(propEui!=='net'){
@@ -184,14 +189,15 @@ define(['angular', 'highcharts', './main'], function(angular) {
               name: propEui,
               id: propEui,
               data: eui[propEui],
-              color:getColor(),
+              color:colors[index++],
               borderWidth: 0
             };
             series.push(modelEui);
-              }
           }
+          }
+          index=0;
           for (var propEnergy in energyUse) {
-            
+
           if(propEnergy!=='net'){
             var modelEnergyUse={
                 name: propEnergy,
@@ -199,7 +205,7 @@ define(['angular', 'highcharts', './main'], function(angular) {
                 stack: 'eui',
                 borderWidth: 0,
                 linkedTo: propEnergy,
-                color: getColor(),
+                color: colors[index++],
                 yAxis: 1
               };
 
