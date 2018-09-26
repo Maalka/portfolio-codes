8a0c4df034dbe91fa7a6deb07dd377bcde0d9cf9
Ariel Kotch <arielkotch@outlook.com>
2018-09-26 19:35:28 -0400

    highcharts width resizes based on window size

--- app/assets/javascripts/common/directives/split.js
+++ app/assets/javascripts/common/directives/split.js
@@ -6,7 +6,7 @@
 define(['angular', './main', 'highcharts'], function(angular) {
   'use strict';
   var mod = angular.module('common.directives');
-  mod.directive('bar', [function() {
+  mod.directive('split', [function() {
     return {
 
       restrict: 'E',
@@ -23,18 +23,16 @@ define(['angular', './main', 'highcharts'], function(angular) {
         categories:'='
 
       },
-      link: function(scope, element){
+      link: function(scope, element) {
+
         var options = {
           chart: {
             type: 'bar',
             marginTop: 50,
             marginBottom: 70,
-            height: scope.height,
-            reflow: false
-
+            height: scope.height
 
           },
-
           yAxis: {
             min: 0,
             gridLineColor: 'transparent',
@@ -46,7 +44,7 @@ define(['angular', './main', 'highcharts'], function(angular) {
             }
           },
       legend:{
-       align: 'right',
+        align: 'right',
        verticalAlign: 'top',
        layout: 'vertical',
             itemStyle: {
@@ -92,31 +90,13 @@ define(['angular', './main', 'highcharts'], function(angular) {
           },
           series: scope.series
         };
-        var chart;
-          angular.element(element).highcharts(options, function () {
-             chart = this;
-          });
-      scope.width = window.innerWidth;
-      scope.height = window.innerHeight;
-      angular.element(window).bind('resize', function(){
-        scope.width = window.innerWidth;
-        scope.height = window.innerHeight;
-        // manuall $digest required as resize event
-        // is outside of angular
-        scope.$digest();
-      });
-      scope.$watch('width',function(width){
-          console.log(width,'width');
-            console.log(chart,'chart');
-          chart.setSize((width/2), (chart.height));
-
-      });
+      angular.element(element).highcharts(options);
       },
       controller: ["$scope","$element",function($scope,$element) {
 
 
         var series = [];
-        var colors = ['#1F2C5C', '#3F58CE', '#5D70D4', '#08B4BB', '#6BD2D6', '#06A1F9', '#0579BB', '#F5B569', '#EB885C', '#D4483D', '#64467D', '#9A6ECE','#06AED5','#564787','#FDE74C'];
+        //var colors = ['#1F2C5C', '#3F58CE', '#5D70D4', '#08B4BB', '#6BD2D6', '#06A1F9', '#0579BB', '#F5B569', '#EB885C', '#D4483D', '#64467D', '#9A6ECE','#06AED5','#564787','#FDE74C'];
         var index;
 
         function createSeries() {
@@ -127,7 +107,18 @@ define(['angular', './main', 'highcharts'], function(angular) {
                 name: propEnergy,
                 id: propEnergy+$scope.options.id,
                 data: $scope.data[propEnergy],
-                color: colors[index++],
+                color: {
+                    pattern: {
+                        path: {
+                            d: 'M 0 0 L 6 6 M 5 -1 L 7 1 M -1 5 L 1 7',
+                            strokeWidth: 3
+                        },
+                        height: 6,
+                        width: 6,
+                        r: 4,
+                        color: '#3F58CE'
+                    }
+                },
                 borderWidth: 0
               };
             if($scope.options.id==='_eui'){
@@ -146,7 +137,6 @@ define(['angular', './main', 'highcharts'], function(angular) {
         $scope.series = series;
         console.log($element);
         $scope.height = $scope.categories.length*10+360;
-        console.log($element,'el');
       }]
     };
   }]);
